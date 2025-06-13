6da38a22081f032b7aec6437ebe2916643e5955c
Angelt11 <angeltorres19e@gmail.com>
2025-06-13 17:50:33 -0600

    se agrego una barra de vida y otras cosas a ambos pokemon
    se crearon scriptable objects para los ataques y vida de los pokemon

---
+++ Assets/Scripts/Fighter.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    [SerializeField]
+    private Health _health;
+    [SerializeField]
+    private Animator _characterAnimator;
+    [SerializeField]
+    private Attacks _attacks;
+    
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private Slider _healthSlider;
+    [SerializeField]
+    private float _initiateHealth = 200f;
+    [SerializeField]
+    private UnityEvent<float> _onUpdateHealth;
+    [SerializeField]
+    private UnityEvent _onDefeated;
+    [SerializeField]
+    private UnityEvent _onTakeDamage;
+    private float _currentHealth;
+    public void InitializeHealth()
+    {
+        _currentHealth = _initiateHealth;
+        UpdateHealth();
+    }
+    private void UpdateHealth()
+    {
+        _onUpdateHealth?.Invoke(_currentHealth / _initiateHealth);
+    }
+    public void TakeDamage(float damage)
+    {
+        _currentHealth -= damage;
+        if (_currentHealth < 0)
+        {
+            _onDefeated?.Invoke();
+            _currentHealth = 0;
+        }
+        else
+        {
+            _onTakeDamage?.Invoke();
+        }
+        UpdateHealth();
+    }
+}
