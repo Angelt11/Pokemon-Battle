cf593a4eb28bd50caeeb88ca3d2d4bdf996f3ca5
Angelt11 <angel.torres@uteca.edu.mx>
2025-06-28 11:20:32 -0600

    se corrigieron errores

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using System.Collections;
-using UnityEditor.Rendering.LookDev;
 
 public class BattleManager : MonoBehaviour
 {
